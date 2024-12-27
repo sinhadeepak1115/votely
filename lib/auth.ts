45715e6064209a6644f763f230dd5e03fef65058
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-12-27 15:02:18 +0530

    added user db

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,16 +1,31 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import prisma from "./db";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
-  callback: {
-    async redirect({ url, baseUrl }) {
-      return baseUrl;
+  callbacks: {
+    async signIn(params) {
+      console.log(params);
+      try {
+        const user = await prisma.user.create({
+          data: {
+            email: params.user.email,
+            name: params.user.name,
+            provider: "GOOGLE",
+            image: params.user.image,
+          },
+        });
+        console.log(user);
+      } catch (e) {
+        console.error(e);
+      }
+      return true;
     },
   },
 };
---
+++ prisma/migrations/20241225132215_dev/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "Provider" AS ENUM ('GOOGLE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "provider" "Provider" NOT NULL,
+    "image" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
